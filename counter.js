4ee5ba714cf3962ca574a5ba9c5079f3b5722b60
Emelogu <emelogualex20@gmail.com>
2022-08-11 21:42:35 +0100

    updated

---
+++ counter.js
@@ -0,0 +1,21 @@
+let count= 0;
+
+document.getElementById("decrease").onclick = function(){
+    count-=1
+     document.getElementById("count").innerHTML =count;   
+}
+      
+document.getElementById("reset").onclick = function(){
+    count =0;
+    document.getElementById("count").innerHTML = count;
+}
+
+
+document.getElementById("increase").onclick = function(){
+    count+= 1;
+    document.getElementById("count").innerHTML= count;
+  
+}
+   
+    
+    
